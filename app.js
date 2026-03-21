fbde508b46fea18dcba06b165c8d9e3172272527
Andr3w <scottwebster.andrew@gmail.com>
2026-03-20 22:50:25 -0700

    Update app.js

--- app.js
+++ app.js
@@ -6,8 +6,6 @@ from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 import { getFirestore, doc, setDoc, collection, getDocs, getDoc, addDoc } 
 from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
-import { db } from "./app.js";
-
 // YOUR CONFIG (same as before)
 const firebaseConfig = {
   apiKey: "AIzaSyApNczevOMrVuPSEL4oLYGgvVr7IYZHHRE",
